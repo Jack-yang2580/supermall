efa4775cad59368fd42434719a19ffe19d51886f
Jack-yang2580 <2236990090@qq.com>
2021-04-15 06:40:29 +0300

    对个人中心的补充

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import toast from 'components/common/toast'
 
 import FastClick from 'fastclick'
+import VueLazyload from "vue-lazyload";
 
 //添加事件总线对象
 Vue.prototype.$bus = new Vue()
@@ -13,6 +14,11 @@ Vue.use(toast)
 
 //解决移动端300ms的延迟
 FastClick.attach(document.body)
+
+//使用图片的懒加载
+Vue.use(VueLazyload,{
+  loading:require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
