0c2092827216108a3523c646b92fdcffd990e6fb
Jack-yang2580 <2236990090@qq.com>
2021-06-08 11:20:55 +0800

    '最后提交'

---
+++ src/views/category/childComps/TabContentDetail.vue
@@ -0,0 +1,32 @@
+<template>
+
+   <grid-view>
+     <good-list-item v-for="(item, index) in categoryDetail" :key="index" :goods="item"></good-list-item>
+   </grid-view>
+
+</template>
+
+<script>
+  // import GoodsListItem from 'views/home/childComps/GoodsListItem'//
+import GoodListItem from "../../../components/content/goods/GoodListItem";
+import GridView from "../../../components/common/gridView/GridView";
+  export default {
+    name: "TabContentDetail",
+    components: {
+      GoodListItem,
+      GridView
+    },
+    props: {
+      categoryDetail: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
